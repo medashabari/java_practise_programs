0e7dc71e809398c1e33afa27037115831a608071
medashabari <72682329+medashabari@users.noreply.github.com>
2021-01-27 20:35:35 +0530

    Add files via upload

---
+++ Quadratic_roots.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.lang.String;
+class Quadratic_roots{
+	public static void main(String[] args) {
+		int a,b,c,d,f=0;
+		Scanner scr = new Scanner(System.in);
+		System.out.println("ENter the values of a,b,c:");
+		a=scr.nextInt();
+		b=scr.nextInt();
+		c=scr.nextInt();
+		d=(b*b)-(4*a*c);
+		if(d==0){
+			System.out.println("Roots are real and imaginary");
+			f=1;
+		}else if(d>0){
+			System.out.println("Roots are real and unequal");
+			f=1;
+		}
+		else 
+			System.out.println("Roots are imaginary");
+		if(f==1){
+			float r1 = (float)(b+Math.sqrt(d))/(2*a);
+			float r2 = (float)(b-Math.sqrt(d))/(2*a);
+			System.out.println("Roots are:   "+r1+" "+r2);
+		}
+	}
+}
